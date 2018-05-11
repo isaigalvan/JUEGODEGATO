f66caa70639e28511d70b980be944f529ffb7809
isaigalvan <isaig2015@gmail.com>
2018-05-10 21:31:23 -0500

    Metodo creado verifica el ganador, text view agregado para mostrar el ganador final

--- app/src/main/java/a15300087/juegodegato/JUEGODEGATO.java
+++ app/src/main/java/a15300087/juegodegato/JUEGODEGATO.java
@@ -1,25 +1,31 @@
 package a15300087.juegodegato;
 
+import android.annotation.SuppressLint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+@SuppressLint("ByteOrderMark")
 public class JUEGODEGATO extends AppCompatActivity implements View.OnClickListener{
 
     public Button b1, b2, b3, b4 ,b5, b6, b7, b8, b9;
     public Button[] matriz;
-    Object[][] lamatriz;
+    int[][] lamatriz;
     boolean turno;
+    int conti;
+    TextView eti;
 
     public void init(){
-        lamatriz = new Object[3][3];
+        lamatriz = new int[3][3];
         for(int cont = 0 ; cont < 3 ; cont ++){
             for(int cont1 = 0 ; cont1 < 3 ; cont1 ++){
                 lamatriz[cont][cont1]= R.drawable.p;
             }
         }
         turno=true;
+        conti = 0;
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +41,7 @@ public class JUEGODEGATO extends AppCompatActivity implements View.OnClickListen
         b7 = (Button)findViewById(R.id.b7);
         b8 = (Button)findViewById(R.id.b8);
         b9 = (Button)findViewById(R.id.b9);
+        eti = (TextView)findViewById(R.id.eti);
         this.init();
         matriz = new Button[]{b1, b2, b3, b4 ,b5, b6, b7, b8, b9};
         for(Button b : matriz){
@@ -42,11 +49,10 @@ public class JUEGODEGATO extends AppCompatActivity implements View.OnClickListen
         }
     }
 
-    @Override
-    public void onClick(View v){
+    public void onClick(View v) {
         Button b = (Button)v;
         this.click(b);
-    }﻿
+    }       ﻿
 
     public void click(Button b){
 
@@ -92,5 +98,91 @@ public class JUEGODEGATO extends AppCompatActivity implements View.OnClickListen
 
         turno =! turno;
         b.setClickable(true);
+        conti ++;
+        this.verificador();
+    }
+
+    public void verificador(){
+        int CXF=0, COF=0, CXC=0, COC=0, CXDP=0, CODP=0, CXDI=0, CODI=0, TERGATO=3;
+        boolean sigano = false;
+
+        for(int cont = 0 ; cont < lamatriz.length ; cont ++){
+            for(int cont1 = 0 ; cont1 < lamatriz.length ; cont1 ++){
+                if(lamatriz[cont][cont1]== R.drawable.x){
+                    CXF++;
+                    if(CXF==3){
+                        sigano=true;
+                    }
+                }
+
+                if(lamatriz[cont][cont1]== R.drawable.o){
+                    COF++;
+                    if(COF==3){
+                        sigano=true;
+                    }
+                }
+
+                if(lamatriz[cont1][cont]== R.drawable.x){
+                    CXC++;
+                    if(CXC==3){
+                        sigano=true;
+                    }
+                }
+
+                if(lamatriz[cont1][cont]== R.drawable.o){
+                    COC++;
+                    if(COC==3){
+                        sigano=true;
+                    }
+                }
+
+                if(cont==cont1){
+                    if(lamatriz[cont][cont1]== R.drawable.x){
+                        CXDP++;
+                        if(CXDP==3){
+                            sigano=true;
+                        }
+                    }
+
+                    if(lamatriz[cont][cont1]== R.drawable.o){
+                        CODP++;
+                        if(CODP==3){
+                            sigano=true;
+                        }
+                    }
+                }
+
+                if((cont+cont1)==TERGATO-1){
+                    if(lamatriz[cont][cont1]== R.drawable.x){
+                        CXDI++;
+                        if(CXDI==3){
+                            sigano=true;
+                        }
+                    }
+
+                    if(lamatriz[cont][cont1]== R.drawable.o){
+                        CODI++;
+                        if(CODI==3){
+                            sigano=true;
+                        }
+                    }
+                }
+            }
+            CXC=0;COC=0;COF=0;CXF=0;
+        }
+        if(sigano){
+            String v="";
+            if(!turno){
+                v = "GANO JUGADOR X";
+            }
+            if(turno){
+                v = "GANO JUGADOR O";
+            }
+            eti.setText(v);
+            conti=0;
+        }
+        if(conti == 9){
+            eti.setText("EMPATE");
+        }
     }
 }
